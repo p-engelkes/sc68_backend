3d83e43fdf80d6f1968919722dbbecf24686d86c
Patrick Engelkes <patrick.engelkes@gmail.com>
2017-01-12 20:31:34 +0100

    BE-3: Finish UserControllerTest.

--- src/test/java/com/pengelkes/controller/UserControllerTest.kt
+++ src/test/java/com/pengelkes/controller/UserControllerTest.kt
@@ -1,5 +1,6 @@
 package com.pengelkes.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.pengelkes.SpringTestCase
 import com.pengelkes.service.User
 import com.winterbe.expekt.should
@@ -38,5 +39,46 @@ class UserControllerTest : SpringTestCase() {
                 .andReturn()
 
         User(mockResult.response.contentAsString).should.equal(databaseUser)
+    }
+
+    @Test
+    fun testUpdateUser() {
+        val updateUser = databaseUser
+        updateUser.firstName = "Peter"
+        updateUser.lastName = "Pan"
+        updateUser.backNumber = 8
+        val json = ObjectMapper().writeValueAsString(updateUser)
+
+        val mockResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/users/1")
+                .contentType(MediaType.APPLICATION_JSON_UTF8).content(json))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andReturn()
+
+        User(mockResult.response.contentAsString).should.equal(updateUser)
+    }
+
+    @Test
+    fun testRegisterUser() {
+        val newUser = User("new@user.com", "test")
+        val json = ObjectMapper().writeValueAsString(newUser)
+
+        val mockResult = mockMvc.perform(MockMvcRequestBuilders.post("/api/users/register")
+                .contentType(MediaType.APPLICATION_JSON_UTF8).content(json))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andReturn()
+
+        mockResult.response.contentAsString.toInt().should.equal(2)
+    }
+
+    @Test
+    fun getAllPositions() {
+        val mockResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/positions"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andReturn()
+
+        val expectedPositions = "[\"GOALKEEPER\",\"DEFENSE\",\"MIDFIELD\",\"OFFENSE\",\"COACH\",\"SUPERVISOR\"]"
+
+        mockResult.response.contentAsString.should.equal(expectedPositions)
     }
 }

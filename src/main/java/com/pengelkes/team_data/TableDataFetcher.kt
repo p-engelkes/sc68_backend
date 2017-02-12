d760abbd19de5a59b1a0f63c6aa228cbcbb3b718
pengelkes <patrick.engelkes@gmail.com>
2017-02-12 18:01:50 +0100

    BE-18: Crawl all tableData (teamName, iconUrl, games, wonGames, tiedGames, lostGames, goalRatio, goalDifference and points).

--- src/main/java/com/pengelkes/team_data/TableDataFetcher.kt
+++ src/main/java/com/pengelkes/team_data/TableDataFetcher.kt
@@ -10,36 +10,107 @@ class TableDataFetcher(val tableData: String) {
         val clubNameStartingDiv = "<div class=\"club-name\">"
         val logoStartString = "data-responsive-image=\"//"
         val logoEndString = "\"></span>"
+        val tableBodyStartTag = "<tbody>"
+        val tableBodyEndTag = "</tbody>"
+        val tableRowStartTag = "<tr"
+        val tableRowEndTag = "</tr>"
+        val tableDataStartTag = "<td"
+        val tableDataEndTag = "</td>"
     }
 
     fun getAllTableTeams(): List<TableTeam> {
         val allTeamsList = mutableListOf<TableTeam>()
-        val allTeamNames = getAllTeamNames().toList()
-        val allTeamIcons = getAllTeamIcons().toList()
-        (0..allTeamNames.count() - 1).mapTo(allTeamsList)
-        { TableTeam(getTeamName(allTeamNames[it].value), allTeamIcons[it].value) }
+        val teamDataList = getAllTeamData()
+
+        teamDataList.forEach {
+            val tableTeam = TableTeam()
+            tableTeam.name = getTeamName(it)
+            tableTeam.icon = getIcon(it)
+            tableTeam.games = getTeamValueBetweenTags(TableTeam.gamesIndex, tableDataStartTag, it).toInt()
+            tableTeam.wonGames = getTeamValueBetweenTags(TableTeam.wonGamesIndex, tableDataStartTag, it).toInt()
+            tableTeam.tiedGames = getTeamValueBetweenTags(TableTeam.tiedGamesIndex, tableDataStartTag, it).toInt()
+            tableTeam.lostGames = getTeamValueBetweenTags(TableTeam.lostGamesIndex, tableDataStartTag, it).toInt()
+            tableTeam.goalRatio = getTeamValueBetweenTags(TableTeam.goalRatioIndex, tableDataStartTag, it)
+            tableTeam.goalDifference = getTeamValueBetweenTags(
+                    TableTeam.goalDifferenceIndex, tableDataStartTag, it).toInt()
+            tableTeam.points = getTeamValueBetweenTags(TableTeam.pointsIndex, tableDataStartTag, it).toInt()
+
+            allTeamsList.add(tableTeam)
+        }
 
         return allTeamsList
     }
 
-    fun getAllTeamNames(): Sequence<MatchResult> {
-        val teamRegex = Regex("<div class=\"club-name\">.*</div>")
-        return teamRegex.findAll(tableData)
+    fun getAllTeamData(): List<String> {
+        val teamDataList = mutableListOf<String>()
+        var sanitizedTableData = getDataBetweenTags(tableData, tableBodyStartTag, tableBodyEndTag)
+        while (sanitizedTableData.contains(tableRowEndTag)) {
+            val teamData = getDataWithTags(sanitizedTableData, tableRowStartTag, tableRowEndTag)
+            sanitizedTableData = sanitizedTableData.replace(teamData, "")
+            teamDataList.add(teamData)
+        }
+
+        return teamDataList
     }
 
     fun getTeamName(teamDiv: String): String {
-        return teamDiv.substring(clubNameStartingDiv.length, teamDiv.indexOf(closingDiv))
-    }
-
-    fun getAllTeamIcons(): Sequence<MatchResult> {
-        val iconRegex = Regex("<div class=\"club-logo.*</div>")
-        return iconRegex.findAll(tableData)
+        val startingDivIndex = teamDiv.indexOf(clubNameStartingDiv)
+        return teamDiv.substring(startingDivIndex + clubNameStartingDiv.length,
+                teamDiv.indexOf(closingDiv, startingDivIndex))
     }
 
     fun getIcon(iconDiv: String): String {
-        return iconDiv.substring(iconDiv.indexOf(logoStartString) + logoStartString.length,
-                iconDiv.indexOf(logoEndString))
+        val startingIconDivIndex = iconDiv.indexOf(logoStartString)
+        return iconDiv.substring(startingIconDivIndex + logoStartString.length,
+                iconDiv.indexOf(logoEndString, startingIconDivIndex))
+    }
+
+    fun getDataWithTags(data: String, startTag: String, endTag: String): String {
+        return data.substring(data.indexOf(startTag), data.indexOf(endTag) + endTag.length)
+    }
+
+    fun getDataBetweenTags(data: String, startTag: String, endTag: String, startIndex: Int? = null): String {
+        var dataString = data
+        startIndex?.let {
+            dataString = dataString.substring(startIndex, dataString.length)
+        }
+
+        return dataString.substring(dataString.indexOf(startTag) + startTag.length, dataString.indexOf(endTag)).trim()
+    }
+
+    fun getValueFromTableDataString(tableDataString: String): String {
+        return tableDataString.substring(tableDataString.indexOf(">") + 1, tableDataString.length)
+    }
+
+    fun findNthIndexOf(n: Int, stringToSearchFor: String, stringToSearchIn: String): Int {
+        var index = 0
+        for (i in 0..n) {
+            index = stringToSearchIn.indexOf(stringToSearchFor, index) + stringToSearchFor.length
+        }
+
+        return index - stringToSearchFor.length
+    }
+
+    fun getTeamValueBetweenTags(numberOfIndex: Int, stringToSearchFor: String, stringToSearchIn: String): String {
+        val valueIndex = findNthIndexOf(numberOfIndex, stringToSearchFor, stringToSearchIn)
+        val valueString = getDataBetweenTags(stringToSearchIn, tableDataStartTag, tableDataEndTag, valueIndex)
+        return getValueFromTableDataString(valueString)
     }
 }
 
-data class TableTeam(val name: String, val icon: String)
+data class TableTeam(var name: String? = null, var icon: String? = null,
+                     var games: Int? = null, var wonGames: Int? = null,
+                     var tiedGames: Int? = null, var lostGames: Int? = null,
+                     var goalRatio: String? = null, var goalDifference: Int? = null,
+                     var points: Int? = null) {
+
+    companion object {
+        val gamesIndex = 3
+        val wonGamesIndex = 4
+        val tiedGamesIndex = 5
+        val lostGamesIndex = 6
+        val goalRatioIndex = 7
+        val goalDifferenceIndex = 8
+        val pointsIndex = 9
+    }
+}
--- src/test/java/com/pengelkes/team_data/TableDataFetcherTest.kt
+++ src/test/java/com/pengelkes/team_data/TableDataFetcherTest.kt
@@ -1,8 +1,8 @@
 package com.pengelkes.team_data
 
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import kotlin.test.assertEquals
 
 /**
  * Created by pengelkes on 11.02.2017.
@@ -10,10 +10,11 @@ import kotlin.test.assertEquals
 class TableDataFetcherTest {
 
     lateinit var tableDataFetcher: TableDataFetcher
+    lateinit var tableDataString: String
 
     @Before
     fun setup() {
-        val tableDataString = javaClass.classLoader.getResource("files/table_data/table_data.txt")
+        tableDataString = javaClass.classLoader.getResource("files/table_data/table_data.txt")
                 .readText(Charsets.UTF_8)
 
         tableDataFetcher = TableDataFetcher(tableDataString)
@@ -27,32 +28,87 @@ class TableDataFetcherTest {
     }
 
     @Test
-    fun getAllTeamNames() {
-        val expectedNumberOfTeams = 14
+    fun getAllTeamData() {
+        val expectedNumberOfDataBlocks = 14
 
-        assertEquals(expectedNumberOfTeams, tableDataFetcher.getAllTeamNames().count())
+        assertEquals(expectedNumberOfDataBlocks, tableDataFetcher.getAllTeamData().size)
     }
 
     @Test
     fun getTeamName() {
-        val expectedTeamName = "Skiclub Nordwest Rheine"
-        val teamDiv = "<div class=\"club-name\">Skiclub Nordwest Rheine</div>"
+        val expectedTeamName = "SpVg Emsdetten 05 II"
+        val teamDiv = javaClass.classLoader.getResource("files/table_data/data_between_tags.txt")
+                .readText(Charsets.UTF_8)
 
         assertEquals(expectedTeamName, tableDataFetcher.getTeamName(teamDiv))
     }
 
     @Test
-    fun getAllTeamIcons() {
-        val expectedNumberOfIcons = 14
+    fun getIcon() {
+        val expectedLogoUrl = "www.fussball.de/export.media/-/action/getLogo/format/3/id/00ES8GN8TC00000JVV0AG08LVUPGND5I/verband/0123456789ABCDEF0123456700004130"
+        val iconDiv = javaClass.classLoader.getResource("files/table_data/data_between_tags.txt")
+                .readText(Charsets.UTF_8)
 
-        assertEquals(expectedNumberOfIcons, tableDataFetcher.getAllTeamIcons().count())
+        assertEquals(expectedLogoUrl, tableDataFetcher.getIcon(iconDiv))
     }
 
     @Test
-    fun getIcon() {
-        val expectedLogoUrl = "www.fussball.de/export.media/-/action/getLogo/format/3/id/00ES8GN8TC00001DVV0AG08LVUPGND5I/verband/0123456789ABCDEF0123456700004130"
-        val iconDiv = "<div class=\"club-logo table-image\"><span data-responsive-image=\"//www.fussball.de/export.media/-/action/getLogo/format/3/id/00ES8GN8TC00001DVV0AG08LVUPGND5I/verband/0123456789ABCDEF0123456700004130\"></span></div>"
+    fun getDataBetweenTags() {
+        val expected = javaClass.classLoader.getResource("files/table_data/sanitized_table_data.txt")
+                .readText(Charsets.UTF_8)
 
-        assertEquals(expectedLogoUrl, tableDataFetcher.getIcon(iconDiv))
+        assertEquals(expected, tableDataFetcher.getDataBetweenTags(
+                tableDataString, TableDataFetcher.tableBodyStartTag, TableDataFetcher.tableBodyEndTag
+        ))
+    }
+
+    @Test
+    fun getDataBetweenTagsStartingAtSpecificIndex() {
+        val expected = ">15"
+        val data = javaClass.classLoader.getResource("files/table_data/data_between_tags.txt")
+                .readText(Charsets.UTF_8)
+
+        assertEquals(expected, tableDataFetcher.getDataBetweenTags(
+                data, TableDataFetcher.tableDataStartTag, TableDataFetcher.tableDataEndTag, 647
+        ))
+    }
+
+    @Test
+    fun getDataWithTags() {
+        val data = javaClass.classLoader.getResource("files/table_data/sanitized_table_data.txt")
+                .readText(Charsets.UTF_8)
+        val expected = javaClass.classLoader.getResource("files/table_data/data_between_tags.txt")
+                .readText(Charsets.UTF_8)
+
+        assertEquals(expected, tableDataFetcher.getDataWithTags(
+                data, TableDataFetcher.tableRowStartTag, TableDataFetcher.tableRowEndTag
+        ))
+    }
+
+    @Test
+    fun getValueFromTableDataString() {
+        val expected = "15"
+
+        assertEquals(expected, tableDataFetcher.getValueFromTableDataString(">15"))
+    }
+
+    @Test
+    fun findNthIndexOf() {
+        val stringToSearchIn = javaClass.classLoader.getResource("files/table_data/sanitized_table_data.txt")
+                .readText(Charsets.UTF_8)
+
+        val expectedIndex = 647
+        assertEquals(expectedIndex,
+                tableDataFetcher.findNthIndexOf(3, stringToSearchFor = "<td", stringToSearchIn = stringToSearchIn))
+    }
+
+    @Test
+    fun getTeamValueBetweenTags() {
+        val expected = "15"
+        val stringToSearchIn = javaClass.classLoader.getResource("files/table_data/data_between_tags.txt")
+                .readText(Charsets.UTF_8)
+
+        assertEquals(expected, tableDataFetcher.getTeamValueBetweenTags(3,
+                TableDataFetcher.tableDataStartTag, stringToSearchIn))
     }
 }

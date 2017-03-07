9dda60dc1fe6ef6ded1eee872f2200f0d76096a1
pengelkes <patrick.engelkes@gmail.com>
2017-03-07 20:16:24 +0100

    SC68-31: Create an old_classes table and all OldClasses to this table. Add an order_number column and an old_class_id column to the teams table.

---
+++ src/test/resources/db/migrations/V1_0_5__add_old_classes_table_and_data.sql
@@ -0,0 +1,24 @@
+CREATE TABLE old_classes (
+  id           SERIAL4 PRIMARY KEY,
+  name         TEXT,
+  order_number INT
+);
+
+INSERT INTO old_classes (name, order_number) VALUES
+  ('Herren', 0),
+  ('A-Junioren', 1),
+  ('B-Junioren', 2),
+  ('C-Junioren', 3),
+  ('D-Junioren', 4),
+  ('E-Junioren', 5),
+  ('F-Junioren', 6),
+  ('G-Junioren', 7);
+
+ALTER TABLE sc68.public.team
+  ADD COLUMN order_number INT;
+ALTER TABLE sc68.public.team
+  ADD COLUMN old_class_id INT;
+ALTER TABLE sc68.public.team
+  ADD CONSTRAINT fk_old_class
+FOREIGN KEY (old_class_id)
+REFERENCES old_classes (id);
